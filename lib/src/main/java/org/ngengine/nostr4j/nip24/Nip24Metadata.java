159deca585593478bb1415fc9126c33413d10da9
Riccardo Balbo <riccardo0blb@gmail.com>
2025-04-05 16:00:37 +0200

    implement nip24 metadata parser

---
+++ lib/src/main/java/org/ngengine/nostr4j/nip24/Nip24Metadata.java
@@ -0,0 +1,260 @@
+/**
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2025, Riccardo Balbo
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.ngengine.nostr4j.nip24;
+
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+import org.ngengine.nostr4j.NostrPool;
+import org.ngengine.nostr4j.NostrSubscription;
+import org.ngengine.nostr4j.event.NostrEvent;
+import org.ngengine.nostr4j.event.SignedNostrEvent;
+import org.ngengine.nostr4j.event.UnsignedNostrEvent;
+import org.ngengine.nostr4j.keypair.NostrPublicKey;
+import org.ngengine.nostr4j.platform.AsyncTask;
+import org.ngengine.nostr4j.platform.Platform;
+import org.ngengine.nostr4j.signer.NostrSigner;
+import org.ngengine.nostr4j.transport.NostrMessageAck;
+import org.ngengine.nostr4j.utils.NostrUtils;
+
+public class Nip24Metadata  {
+    private static final Logger logger = Logger.getLogger(
+        Nip24Metadata.class.getName()
+    );
+    
+    public final Map<String, Object> metadata;
+    private final NostrEvent sourceEvent;
+
+    public Nip24Metadata(NostrEvent source) throws Exception {
+        this.sourceEvent=source;
+        Platform platform = NostrUtils.getPlatform();
+        String content = sourceEvent.getContent();
+        Map<String, Object> meta = platform.fromJSON(content, Map.class);
+        if(meta==null)throw new IllegalArgumentException("Invalid metadata");
+        this.metadata = meta;         
+    }
+
+    public UnsignedNostrEvent toUpdateEvent() throws Exception{
+        UnsignedNostrEvent event = new UnsignedNostrEvent();
+        event.setKind(0);
+        event.setCreatedAt(Instant.now());
+        event.setContent(NostrUtils.getPlatform().toJSON(metadata));
+        return event;
+    }
+
+    public NostrEvent getSourceEvent(){
+        return sourceEvent;
+    }
+
+
+    public String getDisplayName(){
+        String v = NostrUtils.safeString(metadata.get("display_name"));
+        if(v.isEmpty()){
+            v= NostrUtils.safeString(metadata.get("displayName"));
+        }
+        if (v.isEmpty())
+            return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public void setDisplayName(String name) {
+        metadata.put("display_name", name);
+    }
+
+    public String getName(){
+        String v = NostrUtils.safeString(metadata.get("name"));
+        if (v.isEmpty()) {
+            v=NostrUtils.safeString(metadata.get("username"));
+        }
+        if(v.isEmpty())return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public void setName(String name){
+        metadata.put("name", name);
+    }
+
+    public String getAbout() {
+        Object v = metadata.get("about");
+        if (v == null)
+            return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public void setAbout(String about) {
+        metadata.put("about", about);
+    }
+    
+
+    public String getPicture() {
+        Object v = metadata.get("picture");
+        if (v == null)
+            return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public void setPicture(String picture) {
+        metadata.put("picture", picture);
+    }
+
+    public String getWebsite() {
+        Object v = metadata.get("website");
+        if (v == null)
+            return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public void setWebsite(String website) {
+        metadata.put("website", website);
+    }
+
+    public String getBanner() {
+        Object v = metadata.get("banner");
+        if (v == null)
+            return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public void setBanner(String banner) {
+        metadata.put("banner", banner);
+    }
+
+    public boolean isBot() {
+        Object v = metadata.get("bot");
+        if (v == null)
+            return false;
+        return NostrUtils.safeBool(v);
+    }
+
+
+    public void setBot(boolean bot) {
+        metadata.put("bot", bot);
+    }
+
+    /**
+     * use getName() instead
+     */
+    @Deprecated
+    public String getUsername(){
+        Object v = metadata.get("username");
+        if (v == null)
+            return null;
+        return NostrUtils.safeString(v);
+    }
+
+    public Date getBirthday(){
+        Map<String,Object> birthday = (Map<String,Object>)metadata.get("birthday");
+        if(birthday==null)return null;
+        int year = NostrUtils.safeInt(birthday.get("year"));
+        int month = NostrUtils.safeInt(birthday.get("month"));
+        int day = NostrUtils.safeInt(birthday.get("day"));
+        if(year==0||month==0||day==0)return null;
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.YEAR, year);
+        cal.set(Calendar.MONTH, month-1);
+        cal.set(Calendar.DAY_OF_MONTH, day);
+        return cal.getTime();
+    }
+
+    public void setBirthday(Date birthday){
+        if(birthday==null){
+            metadata.remove("birthday");
+            return;
+        }
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(birthday);
+        Map<String,Object> b =  new HashMap<>();
+        b.put("year", cal.get(Calendar.YEAR));
+        b.put("month", cal.get(Calendar.MONTH)+1);
+        b.put("day", cal.get(Calendar.DAY_OF_MONTH));
+        metadata.put("birthday", b);
+    }
+
+    public void setBirthday(int year, int month, int day){
+        Map<String, Object> b = new HashMap<>();
+        b.put("year", year);
+        b.put("month", month);
+        b.put("day", day);
+        metadata.put("birthday", b);
+    }
+
+    public static Nip24Metadata fromEvent( NostrEvent event) throws Exception{
+        return new Nip24Metadata(event);
+    }
+
+    public static AsyncTask<List<Nip24Metadata>> fetchAll(
+            NostrPool pool) {
+        return pool.fetch(new Nip24MetadataFilter(null)).then(evs -> {
+            return evs.stream().map(e -> {
+                try {
+                    return new Nip24Metadata(e);
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            }).toList();
+        });
+    }
+
+    public static AsyncTask<Nip24Metadata> fetch(
+            NostrPool pool,
+            NostrPublicKey pubkey) {
+        return pool.fetch(new Nip24MetadataFilter(pubkey)).then(
+            evs->{
+                SignedNostrEvent event = (SignedNostrEvent)evs.get(0);
+                try {
+                    return new Nip24Metadata(event);
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }        
+        );
+    }
+
+
+    public static AsyncTask<List<NostrMessageAck>> update(
+            NostrPool pool,
+            NostrSigner signer,
+            Nip24Metadata newMetadata
+    ) throws Exception {
+        UnsignedNostrEvent event = newMetadata.toUpdateEvent();
+        SignedNostrEvent signed = signer.sign(event);
+        return pool.send(signed);
+    }
+
+}
