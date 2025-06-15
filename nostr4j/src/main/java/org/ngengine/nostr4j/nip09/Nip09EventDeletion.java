4b7ae56e18936ccfc1e802f3da76dd80fe192b2d
Riccardo Balbo <riccardo0blb@gmail.com>
2025-06-15 15:25:52 +0200

    nip-09

---
+++ nostr4j/src/main/java/org/ngengine/nostr4j/nip09/Nip09EventDeletion.java
@@ -0,0 +1,32 @@
+package org.ngengine.nostr4j.nip09;
+
+import org.ngengine.nostr4j.event.NostrEvent;
+import org.ngengine.nostr4j.event.SignedNostrEvent;
+import org.ngengine.nostr4j.event.UnsignedNostrEvent;
+
+public class Nip09EventDeletion {
+    public static UnsignedNostrEvent createDeletionEvent(String reason, SignedNostrEvent... events){
+        UnsignedNostrEvent event = new UnsignedNostrEvent();
+        event.withKind(5);
+        event.withContent(reason == null ? "" : reason);
+        for (SignedNostrEvent e : events) {
+            NostrEvent.Coordinates c = e.getCoordinates();
+            event.withTag(c.type(), c.coords());
+            event.withTag("k", ""+e.getKind());
+        }
+        return event;
+    }
+
+
+ 
+    public static UnsignedNostrEvent createDeletionEvent(String reason, NostrEvent.Coordinates... coordinates) {
+        UnsignedNostrEvent event = new UnsignedNostrEvent();
+        event.withKind(5);
+        event.withContent(reason == null ? "" : reason);
+        for (NostrEvent.Coordinates c : coordinates) {
+            event.withTag(c.type(), c.coords());
+            event.withTag("k", c.kind());
+        }
+        return event;
+    }
+}
