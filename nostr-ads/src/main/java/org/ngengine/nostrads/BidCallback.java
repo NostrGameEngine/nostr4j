670d991f9eecac35b9a4f1fcb5127abdd74dad26
Riccardo Balbo <riccardo0blb@gmail.com>
2025-07-08 19:27:26 +0200

    move nostr ads to its own repo

--- nostr-ads/src/main/java/org/ngengine/nostrads/BidCallback.java
+++
@@ -1,9 +0,0 @@
-package org.ngengine.nostrads;
-
-import org.teavm.jso.JSFunctor;
-import org.teavm.jso.JSObject;
-
-@JSFunctor
-public interface BidCallback extends JSObject {
-    void onBid(JSObject bidEvent, String error);
-}
